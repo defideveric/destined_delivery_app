9acb1dcc446df3e21d8b4c52a13cfe67d7575e5e
erichudson <defideveric@gmail.com>
2023-12-16 16:57:04 -0500

    Search Section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
+import GoogleMapSection from '@/components/Home/GoogleMapSection';
+import SearchSection from '@/components/Home/SearchSection';
 import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 
 
 export default function Home() {
   return (
-   <div>
-      <UserButton/>
+   <div className='p-6 grid grid-cols-1 md:grid-cols-3 gap-5'>
+      <div>
+        <SearchSection/>
+      </div>
+
+      <div className='col-span-2'>
+        <GoogleMapSection/>
+      </div>
    </div>
   )
 }
